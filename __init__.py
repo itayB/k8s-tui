886537de571f2eb6870c733141af6f9e41b1163c
Itay Bittan <itay.bittan@gmail.com>
2020-08-01 19:19:17 +0300

    cleanup

--- __init__.py
+++ __init__.py
@@ -23,6 +23,8 @@ class ClustersGrid(npyscreen.GridColTitles):
 class VersionSelectForm(npyscreen.Popup):
     def create(self):
         self.name = 'VersionSelectFormbla'
+        # pop_x = self.grid.relx + self.grid.width//2 - pop_width//2
+        # self.show_atx = 200
         # self.nextrelx = 40
 
 
@@ -34,14 +36,7 @@ class KubeApp(npyscreen.NPSAppManaged): #StandardApp):   # NPSApp):
         self.version_form.edit()
 
     def main(self):
-        self.version_form = self.addForm("select-version-form",
-                                         VersionSelectForm,
-                                         # relx=30,
-                                         #    max_width = 100,
-                                         )
-        # self.version_form.max_width = 100
-        self.version_form.display()
-        main_form = npyscreen.Form(name="Clusters Overview",)
+        main_form = npyscreen.Form(name="Clusters Overview v0.1.0",)
         main_form.add(npyscreen.BoxTitle,
                       name="Namespace",
                       relx=2,
@@ -61,7 +56,6 @@ class KubeApp(npyscreen.NPSAppManaged): #StandardApp):   # NPSApp):
                           'recs',
                           'ssapi',
                       ],
-                    # max_height = column_height
                     )
 
         self.grid = main_form.add(ClustersGrid,
@@ -74,17 +68,6 @@ class KubeApp(npyscreen.NPSAppManaged): #StandardApp):   # NPSApp):
         self.grid.add_handlers({curses.ascii.NL: self.do_stuff})
         self.grid.col_titles = ['Service', 'Container', 'Dev', 'Staging', 'Prod EUC', 'Prod USE']
         self.grid.values = []
-        # grid.edit_cell = [0,1]
-        # for x in range(2):
-        #     row = []
-        #     for y in range(5):
-        #         if y == 0:
-        #             row.append('chart-name')
-        #         elif y == 1:
-        #             row.append('latest')
-        #         else:
-        #             row.append("PASS")
-        #     self.grid.values.append(row)
         self.grid.values = [
             ['rcom-server', 'rcom-server', 'latest', '3.7.86', '3.7.86', '3.7.86'],
             ['', 'cache-updater', 'latest', '3.7.86', '3.7.86', '3.7.86'],
@@ -96,32 +79,18 @@ class KubeApp(npyscreen.NPSAppManaged): #StandardApp):   # NPSApp):
         pop_width = 30
         pop_x = self.grid.relx + self.grid.width//2 - pop_width//2
         pop_y = self.grid.rely + self.grid.height//2
-
+        self.version_form = self.addForm("select-version-form", VersionSelectForm, )
+        self.version_form.show_atx = self.grid.relx + self.grid.width//2 - self.version_form.columns//2
+        self.version_form.show_aty = self.grid.rely + self.grid.height//2 
         self.ms = self.version_form.add(npyscreen.TitleSelectOne,
                                         max_height=4,
                                         value=[1,],
                                         name="Pick One",
-                                        values=["Option1","Option2","Option3"],
+                                        values=["3.7.86", "3.7.87", "3.7.88"],
                                         scroll_exit=True
                                         )
 
-        # t = main_form.add(npyscreen.TitleText, name="Text:",)
-        # fn = main_form.add(npyscreen.TitleFilename, name= "Filename:")
-        # fn2 = main_form.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        # dt = main_form.add(npyscreen.TitleDateCombo, name = "Date:")
-        # s  = main_form.add(npyscreen.TitleSlider, out_of=12, name = "Slider")
-        # ml = main_form.add(npyscreen.MultiLineEdit,
-        #            value = """try typing here!\nMutiline text, press ^R to reformat.\n""",
-        #            max_height=5, rely=9)
-        # ms = main_form.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
-        #            values = ["Option1","Option2","Option3"], scroll_exit=True)
-        # ms2= main_form.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Pick Several",
-        #            values = ["Option1","Option2","Option3"], scroll_exit=True)
-
-        # This lets the user interact with the Form.
         main_form.edit()
-
-        # print(ms.get_selected_objects())
 
 
 if __name__ == "__main__":
